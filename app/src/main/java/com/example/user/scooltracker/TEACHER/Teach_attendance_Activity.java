78fc9eadf8cafc09ce6fa049ac7608223a002702
leothayyil <leothayyil@gmail.com>
2018-02-01 13:16:23 +0530

    graphical ui almost

---
+++ app/src/main/java/com/example/user/scooltracker/TEACHER/Teach_attendance_Activity.java
@@ -0,0 +1,15 @@
+package com.example.user.scooltracker.TEACHER;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.user.scooltracker.R;
+
+public class Teach_attendance_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teach_attendance_);
+    }
+}
